2909b03f98750f8b36ea1b7769ab5d409be39a94
RZ-Q <2096832051@qq.com>
2023-11-07 20:00:20 +0800

    fix a numpy bug

--- src/envs/starcraft2.py
+++ src/envs/starcraft2.py
@@ -374,7 +374,7 @@ class StarCraft2Env(MultiAgentEnv):
                 np.flip(
                     np.transpose(
                         np.array(
-                            list(map_info.pathing_grid.data), dtype=np.bool
+                            list(map_info.pathing_grid.data), dtype=np.bool_
                         ).reshape(self.map_x, self.map_y)
                     ),
                     axis=1,
