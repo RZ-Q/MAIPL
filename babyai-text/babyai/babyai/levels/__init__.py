4816fac39fa0f5f166266dbe708a2636e50e3c02
RZ-Q <2096832051@qq.com>
2023-11-24 14:41:54 +0800

    remove babyai-text

--- babyai-text/babyai/babyai/levels/__init__.py
+++
@@ -1,7 +0,0 @@
-from collections import OrderedDict
-
-from . import iclr19_levels
-from . import bonus_levels
-from . import test_levels
-
-from .levelgen import test, level_dict
--- babyai-text/babyai_text/__init__.py
+++
@@ -1,1 +0,0 @@
-from .levels.mixed_levels import *
