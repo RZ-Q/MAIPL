91a9038a832efe7ef47cc29e12b4b096ef5c1343
RZ-Q <2096832051@qq.com>
2023-11-28 23:30:25 +0800

    add pref_qmix

--- src/learners/__init__.py
+++ src/learners/__init__.py
@@ -5,6 +5,7 @@ from .q_learner_differ_preference import QLearner_differ_perference
 from .q_learner_differ import QLearner_differ
 from .max_q_learner_ddpg import DDPGQLearner
 from .maddpg_learner import MADDPGLearner
+from .pref_qmix_learner import Pref_QLearner
 
 REGISTRY = {}
 
@@ -15,3 +16,4 @@ REGISTRY["qdiffer_preference_learner"] = QLearner_differ_perference
 REGISTRY["qdiffer_learner"] = QLearner_differ
 REGISTRY["ddpg"] = DDPGQLearner
 REGISTRY["maddpg"] = MADDPGLearner
+REGISTRY["pref_q_learner"] = Pref_QLearner
