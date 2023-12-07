ba731279bc860ac6a8427ce94999a3c47d4f8195
RZ-Q <2096832051@qq.com>
2023-12-07 14:52:02 +0800

    remove qdiffer preference learner

--- src/learners/__init__.py
+++ src/learners/__init__.py
@@ -1,7 +1,6 @@
 from .q_learner import QLearner
 from .coma_learner import COMALearner
 from .qtran_learner import QLearner as QTranLearner
-from .q_learner_differ_preference import QLearner_differ_perference
 from .q_learner_differ import QLearner_differ
 from .max_q_learner_ddpg import DDPGQLearner
 from .maddpg_learner import MADDPGLearner
@@ -12,7 +11,6 @@ REGISTRY = {}
 REGISTRY["q_learner"] = QLearner
 REGISTRY["coma_learner"] = COMALearner
 REGISTRY["qtran_learner"] = QTranLearner
-REGISTRY["qdiffer_preference_learner"] = QLearner_differ_perference
 REGISTRY["qdiffer_learner"] = QLearner_differ
 REGISTRY["ddpg"] = DDPGQLearner
 REGISTRY["maddpg"] = MADDPGLearner
