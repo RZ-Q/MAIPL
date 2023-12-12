b4060ddf36e4dbcff7ae529f1c268cc4f5d8f617
RZ-Q <2096832051@qq.com>
2023-12-12 13:50:58 +0800

    rename

--- src/learners/__init__.py
+++ src/learners/__init__.py
@@ -4,7 +4,7 @@ from .qtran_learner import QLearner as QTranLearner
 from .q_learner_differ import QLearner_differ
 from .max_q_learner_ddpg import DDPGQLearner
 from .maddpg_learner import MADDPGLearner
-from .pref_qmix_learner import Pref_QLearner
+from .pref_qmix_learner_wo_RM import Pref_QLearner
 from .qdiffer_globalRM_learner import Qdiffer_globalRM_learner
 
 REGISTRY = {}
@@ -15,5 +15,5 @@ REGISTRY["qtran_learner"] = QTranLearner
 REGISTRY["qdiffer_learner"] = QLearner_differ
 REGISTRY["ddpg"] = DDPGQLearner
 REGISTRY["maddpg"] = MADDPGLearner
-REGISTRY["pref_q_learner"] = Pref_QLearner
+REGISTRY["pref_q_learner_wo_RM"] = Pref_QLearner
 REGISTRY["qdiffer_globalRM_learner"] = Qdiffer_globalRM_learner
