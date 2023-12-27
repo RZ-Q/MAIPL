af8a482cc425c5833cd26f6d208a3a3cec60f284
RZ-Q <2096832051@qq.com>
2023-12-27 18:45:25 +0800

    add aloha

--- src/envs/__init__.py
+++ src/envs/__init__.py
@@ -4,6 +4,7 @@ from .multiagentenv import MultiAgentEnv
 from .starcraft2 import StarCraft2Env
 import sys
 import os
+from .aloha import AlohaEnv
 
 from .overcooked_env import OvercookEnv
 
@@ -13,6 +14,7 @@ def env_fn(env, **kwargs) -> MultiAgentEnv:
 REGISTRY = {}
 REGISTRY["sc2"] = partial(env_fn, env=StarCraft2Env)
 REGISTRY["overcooked"] = partial(env_fn, env=OvercookEnv)
+REGISTRY["aloha"] = partial(env_fn, env=AlohaEnv)
 
 if sys.platform == "linux":
     os.environ.setdefault("SC2PATH",
