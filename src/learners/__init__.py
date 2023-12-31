3fd24bf527195da6d6f64828401410cb63561691
RZ-Q <2096832051@qq.com>
2023-12-31 23:38:07 +0800

    add qplex

--- src/learners/__init__.py
+++ src/learners/__init__.py
@@ -1,9 +1,13 @@
 from .q_learner import QLearner
 from .q_learner_differ import QLearner_differ
 from .pref_qmix_learner import Pref_QMIX_learner
+from .q_learner_globalRM import QLearnerGlobalRM
+from .dmaq_qatten_learner import DMAQ_qattenLearner
 
 REGISTRY = {}
 
 REGISTRY["q_learner"] = QLearner
 REGISTRY["qdiffer_learner"] = QLearner_differ
 REGISTRY["pref_qmix_learner"] = Pref_QMIX_learner
+REGISTRY["q_learner_w_globalRM"] = QLearnerGlobalRM
+REGISTRY["dmaq_qatten_learner"] = DMAQ_qattenLearner
