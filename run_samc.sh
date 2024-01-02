ad248b7a07c4ad7f58c4ae3e4129602e5ccf1c34
RZ-Q <2096832051@qq.com>
2024-01-02 17:54:34 +0800

    fix config bug

--- run_samc.sh
+++ run_samc.sh
@@ -1,11 +1,11 @@
 map="6h_vs_8z"
-cuda_device=3
+cuda_device=2
 alog_name="qplex"
 # pos
 for i in {1..5}
 do
     CUDA_VISIBLE_DEVICES=$cuda_device nohup python3 src/main.py \
-    --config=qmix --env-config=sc2 --map_name=$map \
+    --config=$alog_name --env-config=sc2 --map_name=$map \
     with use_tensorboard=True env_args.reward_only_positive=True \
     seed=$i t_max=5050000 epsilon_anneal_time=1000000 \
     save_model=False save_model_interval=200000 \
@@ -13,17 +13,9 @@ do
 done
 # do
 #     CUDA_VISIBLE_DEVICES=$cuda_device nohup python3 src/main.py \
-#     --config=qmix --env-config=sc2 --map_name=$map \
+#     --config=$alog_name --env-config=sc2 --map_name=$map \
 #     with use_tensorboard=True env_args.reward_only_positive=True \
 #     seed=$i \
 #     save_model=False save_model_interval=200000 \
-#     >/dev/null 2>&1 &
-# done
-# do
-#     CUDA_VISIBLE_DEVICES=$cuda_device nohup python3 src/main.py \
-#     --config=qmix --env-config=sc2 --map_name=$map \
-#     with use_tensorboard=True env_args.reward_only_positive=False \
-#     seed=$i t_max=5050000 epsilon_anneal_time=1000000 \
-#     save_model=True save_model_interval=200000 \
 #     >/dev/null 2>&1 &
 # done
